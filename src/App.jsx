8b0118d3574bf7ecb5e6a8c3a25072ad734de64b
Mohamed-gp <mohamedterba6@gmail.com>
2023-10-11 22:32:07 +0100

    started the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.scss";
+
+function App() {
+  return (
+    <>
+      <p className="bg-yellow-400 p-24 md:text-yellow-300 ">bata </p>hello
+    </>
+  );
+}
+
+export default App;
---
+++ src/Component/Header.jsx
@@ -0,0 +1,14 @@
+
+const header = () => {
+  return (
+    <>
+        <header>
+            <div className="container">
+                
+            </div>
+        </header>
+    </>
+  )
+}
+
+export default header
